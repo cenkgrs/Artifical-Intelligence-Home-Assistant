f948a054b873290eb43827088cbbdd5b68039f33
cenkgrs <cenkgrs@gmail.com>
2020-02-20 02:27:32 +0300

    Facial recognition system created for image and live video -Path finding algorithm improved, will be replaced with object detection system tomorrow

---
+++ face_rec.py
@@ -0,0 +1,36 @@
+import io
+import picamera
+import cv2
+import numpy
+
+#Create a memory stream so photos doesn't need to be saved in a file
+stream = io.BytesIO()
+
+#Get the picture 
+with picamera.PiCamera() as camera:
+    camera.resolution = (1280, 720)
+    camera.capture(stream, format='jpeg')
+
+#Convert the picture into a numpy array
+buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
+
+#Now creates an OpenCV image
+image = cv2.imdecode(buff, 1)
+
+#Load a cascade file for detecting faces
+face_cascade = cv2.CascadeClassifier('/home/pi/Oracle/faces.xml')
+
+#Convert to grayscale
+gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+#Look for faces in the image using the loaded cascade file
+faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+
+print ("Found faces:",str(len(faces)))
+
+#Draw a rectangle around every found face
+for (x,y,w,h) in faces:
+    cv2.rectangle(image,(x,y),(x+w,y+h),(255,255,0),2)
+
+#Save the result image
+cv2.imwrite('result.jpg',image)
