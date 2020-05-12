0615df4ef25704c70e98b3a7687c402a752630c1
cenkgrs <cenkgrs@gmail.com>
2020-05-13 02:38:40 +0300

    mileage info taken from cars site

--- scrap_car_data.py
+++ scrap_car_data.py
@@ -2,29 +2,43 @@ from bs4 import BeautifulSoup
 from requests import get
 import pandas as pd
 
-content_list = []
 prices = []
-page_num=1
+mileages = []
+page_num = 1
+
+car = {
+    "price": "",
+    "mileage": "",
+    "year": "",
+    "brand": "",
+}
+
+prices_list = []
+mileages_list = []
+titles_list = []
 
 base_url = f"https://www.cars.com/for-sale/searchresults.action/?page={page_num}&perPage=100&searchSource=PAGINATION&sort=relevance&stkTypId=28881&zc=34110"
 
 print(base_url)
-while page_num < 3:
+while page_num < 2:
 
     response = get(base_url)
     html_soup = BeautifulSoup(response.text, "html.parser")
-    content_list += html_soup.find_all('span', attrs={'class': 'listing-row__price '})
-    print(content_list)
-
+    prices_list += html_soup.find_all('span', attrs={'class': 'listing-row__price '})
+    mileages_list += html_soup.find_all('span', attrs={'class': 'listing-row__mileage'})
+    titles_list += html_soup.find_all('span', attrs={'class': 'listing - row__title'})
 
     page_num += 1
     base_url = f"https://www.cars.com/for-sale/searchresults.action/?page={page_num}&perPage=100&searchSource=PAGINATION&sort=relevance&stkTypId=28881&zc=34110"
 
-for x in content_list:
-    print(x.text)
-    prices.append((((x.text).replace("$", "")).replace(" ", "")).replace("\n", ""))
+    for x in mileages_list:
+        mile = float((((x.text.replace("mi.", "")).replace(" ", "")).replace("\n", "")).replace(",", "."))
+        km = mile * 1.609
+        mileages.append(km)
+    for x in prices_list:
+        prices.append(((x.text.replace("$", "")).replace(" ", "")).replace("\n", ""))
 
-print(prices)
-
+print(len(prices))
+print(len(mileages))
 
 
